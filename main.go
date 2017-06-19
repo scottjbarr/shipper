f67fddff85bed71ae437ec92bbe5c9e8a0e84e28
Scott Barr <scottjbarr@gmail.com>
2017-06-19 10:13:06 +1000

    Don't use explicit local port

--- main.go
+++ main.go
@@ -11,24 +11,10 @@ func main() {
 	server := os.Getenv("SERVER")
 
 	if server == "" {
-		panic("Please set SERVER address e.g. SERVER=127.0.0.0:5000")
+		panic("Please set SERVER address e.g. SERVER=127.0.0.1:50000")
 	}
 
-	// setup the server address
-	serverAddr, err := net.ResolveUDPAddr("udp", server)
-	if err != nil {
-		panic(err)
-	}
-
-	// setup the client address to send from
-	localAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
-	if err != nil {
-		panic(err)
-	}
-
-	// build the connection
-	conn, err := net.DialUDP("udp", localAddr, serverAddr)
-
+	conn, err := net.Dial("udp", server)
 	if err != nil {
 		panic(err)
 	}
@@ -56,10 +42,11 @@ func main() {
 		// message received, write it to the connection
 		_, err := conn.Write(b)
 
-		// unable to write the message. Not much we can do. We don't want to
-		// save the messages for later, because what if you have a ton of
-		// processes saving messages for later (i.e. many containers running
-		// on a host).
+		// unable to write the message, but not much we can do.
+		//
+		// Saving the message(s) for later is dangerous because what if you
+		// have a ton of processes saving messages for later? i.e. many
+		// containers running on a host, all backing up memory.
 		if err != nil {
 			fmt.Printf("ERROR Cannot write to server %v\n", server)
 		}
